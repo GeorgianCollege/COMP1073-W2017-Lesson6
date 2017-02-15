dc7e49db3b0a3732e4327cb8b03bf42c62017ea5
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-02-15 13:55:53 -0500

    added ajax call to load games.json

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,45 +5,48 @@
 
 (function () { // Beginning of the IIFE
 
-  let data = {
-  "games": [
-    {
-      "name": "Fallout 4",
-      "cost": 69.99,
-      "rating": 4.3
-    },
-    {
-      "name": "Overwatch",
-      "cost": 49.99,
-      "rating": 4.5
-    },
-    {
-      "name": "Horizon Zero Dawn",
-      "cost": 69.99,
-      "rating": 4.1
-    }
-  ]
-};
-
-
   switch (document.title) {
     case "Home":
-      let gameListBody = document.getElementById("gameListBody");
 
-      // for each game in data.games repeat
-      data.games.forEach(function(game) {
-        // inject a "template row" inside the dataRows div tag
-        let newRow = document.createElement("tr");
+      let data = {};
 
-        newRow.innerHTML = `
+      // STEP 1 - instantiate an XHR object
+      let XHR = new XMLHttpRequest(); length
+
+      // STEP 2 - open the JSON file
+      XHR.open("GET", "../games.json", true);
+
+      // STEP 3 - send out a call to the XHR object
+      XHR.send(null);
+
+      // STEP 4 - listen for readystate to be 4
+      XHR.onreadystatechange = function () {
+        if ((this.readyState === 4) && (this.status === 200)) {
+          data = JSON.parse(this.responseText);
+
+        }
+      };
+
+      // STEP 5 - wait until data is finished loading before injecting it
+      XHR.addEventListener("load", function () {
+        let gameListBody = document.getElementById("gameListBody");
+
+        // for each game in data.games repeat
+        data.games.forEach(function (game) {
+          // inject a "template row" inside the dataRows div tag
+          let newRow = document.createElement("tr");
+          newRow.innerHTML = `
           <td>${game.name}</td>
           <td class="text-center">${game.cost}</td>
           <td class="text-center">${game.rating}</td>
         `;
+          gameListBody.appendChild(newRow);
+        }, this);
 
-        gameListBody.appendChild(newRow);
+      });
 
-      }, this);
+
+
 
       break;
 
@@ -59,34 +62,34 @@
       let ButtonArray = [HideButton, HalfSizeButton, ThreeQuarterSizeButton, ShowButton];
 
       // loop through the array of butttons
-      ButtonArray.forEach(function(button) {
+      ButtonArray.forEach(function (button) {
         // set an event listener for each button
         button.addEventListener("click", ButtonClick);
       }, this);
 
       // Use one named function, ButtonClick to respond to each of the buttons
-      function ButtonClick(event){
-          // store which button has been clicked in currentButton
-          //let currentButton = event.currentTarget; <- one way of getting a ref to the button
-          let currentButton = event.currentTarget;
-          switch(currentButton.getAttribute("id")) {
-            case "HideButton":
-              FirstProjectImage.style.display = "none";
+      function ButtonClick(event) {
+        // store which button has been clicked in currentButton
+        //let currentButton = event.currentTarget; <- one way of getting a ref to the button
+        let currentButton = event.currentTarget;
+        switch (currentButton.getAttribute("id")) {
+          case "HideButton":
+            FirstProjectImage.style.display = "none";
             break;
-            case "HalfSizeButton":
-              FirstProjectImage.style.maxWidth = "50%";
-              FirstProjectImage.style.display = "block";
+          case "HalfSizeButton":
+            FirstProjectImage.style.maxWidth = "50%";
+            FirstProjectImage.style.display = "block";
             break;
-            case "ThreeQuarterSizeButton":
-              FirstProjectImage.style.maxWidth = "75%";
-              FirstProjectImage.style.display = "block";
+          case "ThreeQuarterSizeButton":
+            FirstProjectImage.style.maxWidth = "75%";
+            FirstProjectImage.style.display = "block";
             break;
-            case "ShowButton":
-              FirstProjectImage.style.display = "block";
-              FirstProjectImage.style.maxWidth = "100%";
+          case "ShowButton":
+            FirstProjectImage.style.display = "block";
+            FirstProjectImage.style.maxWidth = "100%";
             break;
-          }
         }
+      }
 
       break;
 
@@ -97,7 +100,7 @@
       let Message = document.getElementById("Message");
       let SendButton = document.getElementById("SendButton");
 
-      SendButton.addEventListener("click", function(event){
+      SendButton.addEventListener("click", function (event) {
         event.preventDefault();
 
         console.log(FullName);
